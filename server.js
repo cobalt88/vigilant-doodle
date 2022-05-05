09dbd2985f4222d0d8421fc1da829ae902e705cc
cobalt88 <vincentteune@gmail.com>
2022-05-05 15:35:59 -0400

    5.5.22
    built out querry statements

--- server.js
+++ server.js
@@ -23,10 +23,40 @@ app.get('/', (req, res) => {
   })
 });
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-  console.log(rows)
-});
+//select all data on the candidates table
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//   console.log(rows)
+// });
 
+// select data from the candidates table based on the id
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err,row) => {
+//   if(err) {
+//     console.log(err);
+//   }
+//   console.log(row);
+// });
+
+//delete data from the candidates table - important note: ? = a placeholder value.
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err,result) => {
+//   if(err){
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+
+//create candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+//               VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+
+// 404 not found
 app.use((req, res) => {
   res.status(404).end();
 });
