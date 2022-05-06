8895122aa449b21263d62ec23afafa30918e7464
cobalt88 <vincentteune@gmail.com>
2022-05-05 23:18:28 -0400

    5.5.22
    minor bug fixes before moving onto lesson 5

--- routes/apiRoutes/candidateRoutes.js
+++ routes/apiRoutes/candidateRoutes.js
@@ -5,7 +5,8 @@ const inputCheck = require('../../utils/inputCheck');
 
 
 router.get('/candidates', (req, res) => {
-  const sql = `SELECT candidates.*, parties.name 
+  const sql = 
+  `SELECT candidates.*, parties.name 
   AS party_name 
   FROM candidates 
   LEFT JOIN parties 
@@ -26,12 +27,12 @@ router.get('/candidates', (req, res) => {
 // Get a single candidate based on id#
 router.get('/candidate/:id', (req, res) => {
   const sql =
-    `SELECT candidates.*, parties.name 
-AS party_name 
-FROM candidates 
-LEFT JOIN parties 
-ON candidates.party_id = parties.id 
-WHERE candidates.id = ?`;
+  `SELECT candidates.*, parties.name 
+  AS party_name 
+  FROM candidates 
+  LEFT JOIN parties 
+  ON candidates.party_id = parties.id 
+  WHERE candidates.id = ?`;
   const params = [req.params.id];
 
   db.query(sql, params, (err, row) => {
